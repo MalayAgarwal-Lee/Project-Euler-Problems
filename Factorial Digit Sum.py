c68a186da9d3019ea17338df1fdd63e293364017
Malay Agarwal <malay.agarwal261016@outlook.com>
2017-12-03 00:55:47 +0530

    Factorial Digit Sum.py

--- Factorial Digit Sum.py
+++ Factorial Digit Sum.py
@@ -8,18 +8,15 @@ For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
 and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
+
+Solution logic:
+This is a one-liner.
+I compute the factorial using the reduce() function by multiplying all numbers between 1 and 100, both inclusive. This could have also been done using the fact() function available in the math module but I used this.
+I cast the result into a string to make it iterable. I use the reduce() function on this string to add each digit of the result and print the result.
 '''
 
 
 def main():
-
-    '''
-    This is a one-liner.
-    I compute the factorial using the reduce() function by multiplying all numbers between 1 and 100, both inclusive. This could have also been done using the fact() function available in the math module but I used this.
-    I cast the result into a string to make it iterable. I use the reduce() function on this string to add each digit of the result and print the result.
-    :return:
-    '''
-
     print(reduce(lambda x, y: int(x) + int(y), str(reduce(lambda x, y: x * y, range(1, 101)))))
 
 
